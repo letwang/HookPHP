e129089dd3ff606fa32a6bd03a9054d69138ea05
letwang <498936940@qq.com>
2017-10-01 16:40:14 +0800

    leixingyueshu

--- vendor/letframework/let-db/src/Db.php
+++ vendor/letframework/let-db/src/Db.php
@@ -109,7 +109,7 @@ class Db extends PDO
         //
     }
 
-    public static function getConnection($target = 'master', $key = 'default')
+    public static function getConnection($target = 'master', $key = 'default'):self
     {
         if (isset(self::$connections[$target][$key])) {
             return self::$connections[$target][$key];
@@ -124,14 +124,14 @@ class Db extends PDO
      * @param int $type
 * {
 *       \PDO::FETCH_KEY_PAIR   返回 以第一列为KEY，第二列为VALUE的 数据结构，如果KEY重复，则自动去重且保留最后一个KEY的VALUE
-*       \PDO::FETCH_UNIQUE    返回 以第一列为KEY，其余列为数组VALUE的 数据结构，如果KEY重复，则自动去重且保留最后一个KEY的数组VALUE
+*       \PDO::FETCH_UNIQUE     返回 以第一列为KEY，其余列为数组VALUE的 数据结构，如果KEY重复，则自动去重且保留最后一个KEY的数组VALUE
 *       \PDO::FETCH_GROUP      返回 以第一列为KEY，其余列为二数组VALUE的 数据结构，如果KEY重复，则其值自动加入二维数组中
 * }
      * @return array
      */
     public function fetchAll(string $statement, array $parameters = [], $type = \PDO::FETCH_ASSOC):array
     {
-        $rs = $this->query($statement, $parameters);
+        $rs = $this->_query($statement, $parameters);
         $results = $rs->fetchAll($type);
         $rs->closeCursor();
         $rs = null;
@@ -149,7 +149,7 @@ class Db extends PDO
      */
     public function fetch($statement, array $parameters = [], $type = \PDO::FETCH_ASSOC)
     {
-        $rs = $this->query($statement, $parameters);
+        $rs = $this->_query($statement, $parameters);
         $results = $rs->fetch($type);
         $rs->closeCursor();
         $rs = null;
@@ -167,7 +167,7 @@ class Db extends PDO
      */
     public function fetchColumn($statement, array $parameters = [], $column = 0)
     {
-        $rs = $this->query($statement, $parameters);
+        $rs = $this->_query($statement, $parameters);
         $results = $rs->fetchColumn($column);
         $rs->closeCursor();
         $rs = null;
@@ -175,7 +175,45 @@ class Db extends PDO
         return $results;
     }
 
-    public function query($statement, array $parameters = [])
+    /**
+     * @param string $statement
+     * @param array $parameters
+     * @return array
+     */
+    public function insert($statement, $parameters = []):array
+    {
+        return [
+            'rowCount' => $this->_query($statement, $parameters, $parameters)->rowCount(),
+            'lastInsertId' => $this->lastInsertId()
+        ];
+    }
+
+    /**
+     * @param string $statement
+     * @param array $parameters
+     * @return int
+     */
+    public function update($statement, $parameters = []):int
+    {
+        return $this->_query($statement, $parameters)->rowCount();
+    }
+
+    /**
+     * @param string $statement
+     * @param array $parameters
+     * @return int
+     */
+    public function delete($statement, $parameters = []):int
+    {
+        return $this->_query($statement, $parameters)->rowCount();
+    }
+
+    /**
+     * @param string $statement
+     * @param array $parameters
+     * @return PDOStatement
+     */
+    private function _query(string $statement, array $parameters = [])
     {
         $rs = $this->prepare($statement);
         if ($rs) {
