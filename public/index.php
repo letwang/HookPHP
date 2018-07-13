a34dd26a144248f46103d9a769af89ee2e2dacad
letwang <498936940@qq.com>
2018-07-13 15:24:23 +0800

    业务分布目录

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 define('APP_PUBLIC', getcwd());
 chdir(dirname(__DIR__));
 define('APP_ROOT', getcwd());
-define('APP_PATH', APP_ROOT . '/app/store');
+define('APP_PATH', APP_ROOT . '/app/Store');
 define('APP_CONFIG', require APP_PATH . '/config/default.php');
 
 $app = new Yaf\Application(APP_CONFIG);
